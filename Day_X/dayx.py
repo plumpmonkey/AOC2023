086a669f3c161ede9a194845f6f4d7246ed14741
Jon White <white.jon@gmail.com>
2023-12-02 12:27:24 +0000

    Update template code

--- Day_X/dayx.py
+++ Day_X/dayx.py
@@ -4,14 +4,30 @@ import os
 dirname = os.path.dirname(__file__)
 input_file = os.path.join(dirname, 'sample.txt')
 
+# Define the colours used for text printing
+class Colours(Enum):
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    BOLD = "\033[1m"
+    NORMAL = "\033[0m"
+
 def part1(data):
-    print("Part 1")
+    print()
+    print(f'{Colours.BOLD.value}Part 1')
+    print(f'======{Colours.NORMAL.value}')
 
     return 
 
 
 def part2(data):
-    print("Part 2")
+    print()
+    print(f'{Colours.BOLD.value}Part 2')
+    print(f'======{Colours.NORMAL.value}')
 
     return
 
