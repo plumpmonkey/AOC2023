8fd8b17b289f9fabf07f7d47c58d6ca0981a0f27
Jon White <white.jon@gmail.com>
2023-12-03 16:14:41 +0000

    Day 3 complete

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -83,12 +83,12 @@ def part1(engine_schematic: Schematic):
     print(f'{Colours.BOLD.value}Symbols locations:{Colours.NORMAL.value} {symbol_locations}')
 
     for symbol_location in symbol_locations:
-        print()
-        print(f'{Colours.BOLD.value}Symbol location:{Colours.NORMAL.value} {symbol_location}')
+        # print()
+        # print(f'{Colours.BOLD.value}Symbol location:{Colours.NORMAL.value} {symbol_location}')
 
         # Find the adjacent points for each location
         adjacent_points = symbol_location.neighbours()
-        print(f'{Colours.BOLD.value}Adjacent points:{Colours.NORMAL.value} {adjacent_points}')
+        # print(f'{Colours.BOLD.value}Adjacent points:{Colours.NORMAL.value} {adjacent_points}')
 
         for point in adjacent_points:
             # Check that this point is within the grid
@@ -99,7 +99,7 @@ def part1(engine_schematic: Schematic):
                 # If the value is a digit, then add it to the list
                 if value.isdigit():
                     part_number_locations.append(point)
-                    print(f'{Colours.BOLD.value}Part number location:{Colours.NORMAL.value} {point}')
+                    # print(f'{Colours.BOLD.value}Part number location:{Colours.NORMAL.value} {point}')
                 
     # print()
     # print(f'{Colours.BOLD.value}All part number locations:{Colours.NORMAL.value} {part_number_locations}')
@@ -138,13 +138,77 @@ def part1(engine_schematic: Schematic):
     print(f'{Colours.BOLD.value}Part numbers sum:{Colours.NORMAL.value} {part_numbers_sum}')
 
 
-    return 
+    return symbol_locations, part_number_ranges
+    
 
-
-def part2(data):
+def part2(engine_schematic, symbol_locations, part_number_ranges):
     print()
     print(f'{Colours.BOLD.value}Part 2')
     print(f'======{Colours.NORMAL.value}')
+
+    # Loop through the symbol locations and find the symbols that represent the gears '*'
+    gear_locations = [point for point in symbol_locations if engine_schematic.get_value(point) == '*']
+    print(f'{Colours.BOLD.value}Gears:{Colours.NORMAL.value} {gear_locations}')
+
+    # For each gear, find the adjacent points and see if this exists in the part_number_ranges
+    # If it is, log this as range for this gear.
+    sum_of_gear_ratios = 0
+
+    for gear_location in gear_locations:
+        local_gear_ranges = set()
+        local_gears = set()
+
+        print(f'{Colours.YELLOW.value}Gear location:{Colours.NORMAL.value} {gear_location}')
+        for point in gear_location.neighbours():
+            print(f'{Colours.BLUE.value}Point:{Colours.NORMAL.value} {point}')
+            # Check if this point is in a part number range
+            # Use only the part_number ranges that match the y coordinate neighbour point
+            matching_ranges = [part_number_range for part_number_range in part_number_ranges if part_number_range[0].y == point.y]
+            print(f'{Colours.BOLD.value}Matching ranges:{Colours.NORMAL.value} {matching_ranges}')
+
+            # For each matching range tuple, expand out the ((StartX,StartY),(EndX,EndY)) into a list of points in that range.
+            matching_points = []
+            for matching_range in matching_ranges:
+                # Get the start and end points
+                start_point = matching_range[0]
+                end_point = matching_range[1]
+
+                # Loop through the range and add each point to the list
+                for x in range(start_point.x, end_point.x + 1):
+                    matching_points.append(Point(x, start_point.y))
+
+            print(f'{Colours.BOLD.value}Matching points:{Colours.NORMAL.value} {matching_points}')
+
+            # Check if the gear neighbour point is in the matching points list
+            if point in matching_points:
+                # If it is, then add the gear to the set
+                local_gears.add(point)
+                print(f'{Colours.GREEN.value}Adding local gear :{Colours.NORMAL.value} {point}')
+
+        # expand out the local_gears into a list of ranges and add them to the local_gear_ranges set
+        for local_gear in local_gears:
+            local_gear_ranges.add(engine_schematic.get_part_number_location_range(local_gear))
+
+
+        # If there are 2 ranges in the set, and only 2 elements, then work out the gear ration
+        if len(local_gear_ranges) == 2:
+            # Obtain the part numbers for each range
+            part_numbers = []
+            for local_gear_range in local_gear_ranges:
+                part_numbers.append(engine_schematic.get_full_part_number(local_gear_range))
+
+            print(f'{Colours.RED.value}Part numbers:{Colours.NORMAL.value} {part_numbers}')           
+
+            # Work out the gear ratio
+            gear_ratio = part_numbers[0] * part_numbers[1]
+
+            print(f'{Colours.BOLD.value}Gear ratio:{Colours.NORMAL.value} {gear_ratio}')
+
+            # Add this to the sum of gear ratios
+            sum_of_gear_ratios += gear_ratio
+            
+    print()
+    print(f'{Colours.BOLD.value}Sum of gear ratios:{Colours.NORMAL.value} {sum_of_gear_ratios}')
 
     return
 
@@ -163,8 +227,9 @@ def main():
         print(repr(engine_schematic))
         print(engine_schematic) 
 
-        part1(engine_schematic)
-        part2(engine_schematic)
+        # Call Part 1 and save the symbol_locations & part_number_ranges for Part 2
+        symbol_locations, part_number_ranges = part1(engine_schematic)
+        part2(engine_schematic, symbol_locations, part_number_ranges)
 
 if __name__ == "__main__":
     main()
