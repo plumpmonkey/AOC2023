de0390d42a8d18da712e214c10b20be61e24060f
Jon White <white.jon@gmail.com>
2023-12-05 19:18:37 +0000

    Day 5 progress

--- Day_5/day5.py
+++ Day_5/day5.py
@@ -17,10 +17,23 @@ class Colours(Enum):
     BOLD = "\033[1m"
     NORMAL = "\033[0m"
 
-def part1(data):
+def part1(seeds, maps, num_maps, map_names):
     print()
     print(f'{Colours.BOLD.value}Part 1')
     print(f'======{Colours.NORMAL.value}')
+
+    for seed in seeds:
+        print(f'{Colours.YELLOW.value}Seed:{Colours.NORMAL.value} {seed}')
+
+        for map_num in range(num_maps):
+            print(f'\t{Colours.YELLOW.value}Map:{Colours.NORMAL.value} {map_num+1} - {map_names[map_num]}')
+
+            # Index into the maps list to get the current map
+            for value in maps[map_num]:
+                print(f"\t\t{value} - Range = {value[1]} to {value[1] + value[2]}")
+                if value[1] <= seed < value[1] + value[2]:
+                    print(f"\t\t\t{Colours.GREEN.value}Value {seed} Found{Colours.NORMAL.value}")
+                    break
 
     return 
 
@@ -32,6 +45,47 @@ def part2(data):
 
     return
 
+def process_data(data):
+
+    seeds = []
+    maps = []
+    current_map = []
+    map_names = []
+
+    for line in data:
+        line = line.strip()
+        if not line:
+            continue  # Skip empty lines
+
+        if line.startswith("seeds:"):
+            # Extracting seeds data
+            seed_data = line.split(":")[1].strip()
+            seeds = [int(seed) for seed in seed_data.split()]
+        elif line.endswith("map:"):
+            # Start of a new map category
+            map_names.append(line.split(":")[0].strip())
+
+            if current_map:
+                # Save the previous map before starting a new one
+                maps.append(current_map)
+                current_map = []
+
+        else:
+            # Add data to the current map
+            current_map.append([int(x) for x in line.split()])
+
+    # Add the last map if it exists
+    if current_map:
+        maps.append(current_map)
+
+    print(f'{Colours.BOLD.value}Seeds:{Colours.NORMAL.value} {seeds}')
+    print(f'{Colours.BOLD.value}Maps:{Colours.NORMAL.value} {maps}')
+
+    # print out how many maps we have
+    num_maps = len(maps)
+
+    return seeds, maps, num_maps, map_names
+
 
 def main():
     # Work out the current day based on the current file name
@@ -42,27 +96,9 @@ def main():
     with open(input_file) as f:
         data = f.read().splitlines()
 
-        processed_data = {}
-        current_key = None
+        seeds, maps, num_maps, map_names = process_data(data)
 
-        for line in data:
-            if not line:
-                continue  # Skip empty lines
-
-            if line.endswith("map:") or line == "seeds:":
-                current_key = line
-                processed_data[current_key] = []
-            elif current_key:
-                try:
-                    processed_data[current_key].append([int(x) for x in line.split()])
-                except ValueError as e:
-                    print(f"Error processing line '{line}': {e}")
-            else:
-                print(f"Unrecognized line format or missing category header: {line}")
-
-        print(processed_data)
-
-        part1(data)
+        part1(seeds, maps, num_maps, map_names) 
         part2(data)
 
 if __name__ == "__main__":
