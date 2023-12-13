9a315a010dbf04e7dce5419f7268dc9d5ddcc0f5
Jonathan White <Jonathan6.White@uwe.ac.uk>
2023-12-13 17:51:21 +0000

    Day 11 code complete

--- Day_11/README.md
+++ Day_11/README.md
@@ -6,6 +6,14 @@
 
 ### Part Two
 
+As we need to expand the universe `1,000,000` for each row and column that needs expanding, we cant do this the same as Part 1. We need to use a different approach.
+
+This time
+
+ - determine the original point values before expansion. 
+ - determine which column and rows need expanding
+ - if the points are between the expanding rows and columns, then they need to be expanded in the point list directly
+
 ## Part One - Problem Description
 
 You continue following signs for "Hot Springs" and eventually come across an observatory. The Elf within turns out to be a researcher studying cosmic expansion using the giant telescope here.
@@ -108,3 +116,11 @@ In this example, after expanding the universe, the sum of the shortest path betw
 Expand the universe, then find the length of the shortest path between every pair of galaxies. **What is the sum of these lengths?**
 
 ## Part Two - Problem Description
+
+The galaxies are much older (and thus much farther apart) than the researcher initially estimated.
+
+Now, instead of the expansion you did before, make each empty row or column one million times larger. That is, each empty row should be replaced with `1000000` empty rows, and each empty column should be replaced with `1000000` empty columns.
+
+(In the example above, if each empty row or column were merely `10` times larger, the sum of the shortest paths between every pair of galaxies would be `1030`. If each empty row or column were merely `100` times larger, the sum of the shortest paths between every pair of galaxies would be `8410`. However, your universe will need to expand far beyond these values.)
+
+Starting with the same initial image, expand the universe according to these new rules, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
--- Day_11/day11.py
+++ Day_11/day11.py
@@ -3,6 +3,7 @@ import os
 from enum import Enum
 from helpers import Grid, Point, Colours
 from itertools import combinations
+import copy
 
 dirname = os.path.dirname(__file__)
 input_file = os.path.join(dirname, 'input.txt')
@@ -17,7 +18,7 @@ def part1(universe: Grid):
     print(f'{Colours.BOLD.value}Part 1')
     print(f'======{Colours.NORMAL.value}')
 
-    print(universe)
+    # print(universe)
     print()
     # Universe expansion
     # Loop through the rows in the universe and if it contains all empty data
@@ -40,8 +41,8 @@ def part1(universe: Grid):
     universe.transpose()
 
     print(universe.__repr__())
-    print(f'{Colours.BOLD.value}Final Universe{Colours.NORMAL.value}')
-    print(universe)
+    # print(f'{Colours.BOLD.value}Final Universe{Colours.NORMAL.value}')
+    # print(universe)
 
     galaxies = []
     # Find all the points in the universe that are "#" chars
@@ -66,11 +67,72 @@ def part1(universe: Grid):
     return 
 
 
-def part2(data):
+def part2(universe2: Grid):
     print()
     print(f'{Colours.BOLD.value}Part 2')
     print(f'======{Colours.NORMAL.value}')
 
+
+    # Determine the starting galaxy locations
+    # Find all the points in the universe that are "#" chars
+    galaxies = []
+    for point in universe2.all_points():
+        if universe2.get_value(point) == "#":
+            galaxies.append(point)
+
+    print(f'{Colours.BOLD.value}Galaxies{Colours.NORMAL.value} - {len(galaxies)}')
+    print(f'{Colours.BOLD.value}Galaxy locations{Colours.NORMAL.value} - {galaxies}')
+
+    # Find all the combinations of galaxies
+    galaxy_combinations = list(combinations(galaxies, 2))
+
+    print(f'{Colours.BOLD.value}Galaxy Combinations{Colours.NORMAL.value} - {len(galaxy_combinations)}')
+    print(f'{Colours.BOLD.value}Galaxy Combinations{Colours.NORMAL.value} - {galaxy_combinations}')
+
+    columns_to_expand = []
+    rows_to_expand = []
+
+    # find the columns and rows that need to be expanded
+    for row_index, row in enumerate(universe2.rows_as_str()):
+        if check_all_dots(row):
+            rows_to_expand.append(row_index)
+
+    universe2.transpose()
+
+    for row_index, row in enumerate(universe2.rows_as_str()):
+        if check_all_dots(row):
+            columns_to_expand.append(row_index)
+
+    print(f'{Colours.BOLD.value}Rows to expand{Colours.NORMAL.value} - {rows_to_expand}')
+    print(f'{Colours.BOLD.value}Columns to expand{Colours.NORMAL.value} - {columns_to_expand}')
+
+    # for each column to expand, loop through the galaxy combinations and check if the column is between the two galaxies
+    # if it is, then add 10 to the 2nd point x value
+    point_distance = {}
+    for galaxy_a, galaxy_b in galaxy_combinations:
+            # Determine manhattan distance between the two galaxies
+            point_distance[(galaxy_a, galaxy_b)] = universe2.manhattan_distance(galaxy_a, galaxy_b)
+
+            if galaxy_a.x == 4 and galaxy_a.y == 0 and galaxy_b.x == 9 and galaxy_b.y == 10:
+                print(f'{Colours.BOLD.value}Galaxy A{Colours.NORMAL.value} - {galaxy_a}')
+                print(f'{Colours.BOLD.value}Galaxy B{Colours.NORMAL.value} - {galaxy_b}')
+            
+                print(f'{Colours.BOLD.value}Point Distance{Colours.NORMAL.value} - {point_distance[(galaxy_a, galaxy_b)]}')
+
+            for column in columns_to_expand:
+                if min(galaxy_a.x, galaxy_b.x) < column < max(galaxy_a.x, galaxy_b.x):
+                    point_distance[(galaxy_a, galaxy_b)] += 1000000 - 1
+            
+            for row in rows_to_expand:
+                if min(galaxy_a.y, galaxy_b.y) < row < max(galaxy_a.y, galaxy_b.y):
+                    point_distance[(galaxy_a, galaxy_b)] += 1000000 - 1
+    
+
+    print(f'{Colours.BOLD.value}Point Distance{Colours.NORMAL.value} - {point_distance}')
+    # sum all the points_distance values
+    total_distance = sum(point_distance.values())
+
+    print(f'{Colours.YELLOW.value}Total Distance{Colours.NORMAL.value} - {total_distance}')
     return
 
 
@@ -84,9 +146,11 @@ def main():
         data = f.read().splitlines()
 
         universe = Grid(data)
+        universe2 = copy.deepcopy(universe)
 
         part1(universe)
-        part2(data)
+
+        part2(universe2)
 
 if __name__ == "__main__":
     main()
