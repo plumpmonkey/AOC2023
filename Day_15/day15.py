98952aa653dbb962f16f6684326b8f83d57038d3
Jon White <white.jon@gmail.com>
2023-12-19 15:15:24 +0000

    Day 15 Part 2

--- Day_15/day15.py
+++ Day_15/day15.py
@@ -5,6 +5,54 @@ from enum import Enum
 dirname = os.path.dirname(__file__)
 input_file = os.path.join(dirname, 'input.txt')
 
+
+class Box():
+
+    def __init__(self, num: int):
+        self.box_num = num
+        self.lenses: list[tuple[str, int]] = []
+        self.lens_set = set()
+    
+    def lens_in_box(self, lens: str) -> bool:
+        return lens in self.lens_set
+
+    def update_lens(self, lens: str, focal_length: int):
+        # Find the location of the lens with the given label
+        # and update the focal length
+        loc = self.lens_location(lens)
+        self.lenses[loc] = (lens, focal_length)
+        
+
+    # Return the location in the list of the lens with the given label
+    # Note - must exist, otherwise raises ValueError
+    def lens_location(self, lens_to_del: str) -> int:
+        for i, (lens, focal_length) in enumerate(self.lenses):
+            if lens == lens_to_del:
+                return i
+        
+        raise ValueError(f'No lens with label {lens} found in box {self.box_num}')
+
+    def add_lens(self, lens: str, focal_length: int):
+        self.lenses.append((lens, focal_length))
+        self.lens_set.add(lens)
+
+    def remove_lens(self, lens: str):
+        if self.lens_in_box(lens):
+            # Find lens location in the list
+            loc = self.lens_location(lens)
+            self.lenses.pop(loc)
+            self.lens_set.remove(lens)
+            
+    def focusing_power(self) -> int:
+        power = 0
+
+        for i, (lens, focal_length) in enumerate(self.lenses):
+            # Power is box_num + 1 * (slot index + 1) * focal_length
+            power += (self.box_num + 1) * (i + 1) * int(focal_length)
+
+    
+        return power
+
 # Define the colours used for text printing
 class Colours(Enum):
     RED = "\033[31m"
@@ -50,11 +98,45 @@ def part1(inputs: list):
     return 
 
 
-def part2(data):
+def part2(inputs: list):
     print()
     print(f'{Colours.BOLD.value}Part 2')
     print(f'======{Colours.NORMAL.value}')
 
+    # Create the 256 boxes as a dict with key of the box num
+    boxes = {i: Box(i) for i in range(256)}
+
+    for input in inputs:
+        # Determine the operation character
+        operation = "=" if "=" in input else "-"
+
+        # Split on the operation character
+        lens, focal_length = input.split(operation)
+
+        # Compute the hash of the string
+        hash_val = hash_routine(lens)
+
+        # Get the box for this hash value
+        box = boxes[hash_val]
+
+        # If the operation is to add a lens
+        if operation == "=":
+            if box.lens_in_box(lens):
+                # Label already exists in this box. Update the focal length with the new value
+                box.update_lens(lens, focal_length)
+            else:
+                # Label doesn't exist in this box. Add it to the box to the end
+                box.add_lens(lens, focal_length)
+        elif operation == "-":
+            # Remove the lens from the box
+            box.remove_lens(lens)
+
+    # sum the focusing power of all the boxes
+    power = 0
+    for box in boxes.values():
+        power += box.focusing_power()
+        
+    print(f'The total focusing power is: {power}')
     return
 
 
